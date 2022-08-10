5a19931dacbabde7e6ffb7dee315ddf8bf1de326
Prince <princekrdss2018@gmail.com>
2022-08-10 17:25:15 +0530

    The Level 3 is done

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require('express');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+// const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 
 
 const app = express();
@@ -21,7 +22,7 @@ const userSchema = new mongoose.Schema({
 });
 
 // const secret = "ThisisourlittleSecrets";
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
+// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -39,9 +40,10 @@ app.get("/register", function (req, res) {
 });
 
 app.post("/register", function (req, res) {
+
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     // Now save the data , and redirecting to the secrets Page.
